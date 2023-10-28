55764e0edacfdfb7fad5b73f746fa5674f11fa2a
Samarión Júnior <samarion2@hotmail.com>
2023-10-28 14:18:08 -0300

    second commit

---
+++ src/ToDo.jsx
@@ -0,0 +1,277 @@
+import './ToDo.css';
+
+const ToDo = () => {
+  return (
+    <div className="ToDo">
+      <div className="Painel">
+        <div className="Form blink">
+          <input type="text" className='TaskNameInput'/>
+        </div>
+        <div className="Tasks">
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+          <div className="Task">
+            <button className="CompletTask">
+              <i class="fa-solid fa-check"/>
+            </button>
+            <div className="NameTask">
+              <p className='NameTaskText'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias voluptatem esse consequatur laudantium mollitia harum fugit, inventore debitis facilis in consequuntur aliquid voluptatibus iure odit quia quisquam atque temporibus? Modi!
+              </p>
+            </div>
+            <button className="DeleteTask">
+              <i class="fa-solid fa-trash"/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ToDo;
